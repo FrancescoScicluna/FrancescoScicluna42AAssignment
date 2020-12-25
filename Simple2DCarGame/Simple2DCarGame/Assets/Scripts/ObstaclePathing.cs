26790a06cda1b63480361f1921c378f6e28cdc20
FrancescoScicluna <73109749+FrancescoScicluna@users.noreply.github.com>
2020-12-25 16:05:08 +0100

    Added Waypoints, Car Obstacle Moves

---
+++ Simple2DCarGame/Simple2DCarGame/Assets/Scripts/ObstaclePathing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float obstacleMoveSpeed = 2f;
+
+    int waypointIndex = 0;
+
+    private void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    private void Update()
+    {
+        ObstacleMove();
+    }
+
+    private void ObstacleMove()
+    {
+        if(waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            targetPosition.z = 0f;
+            var obstacleMovement = obstacleMoveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, obstacleMovement);
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
